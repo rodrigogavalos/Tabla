764424aa6ccaa5128085755e8dd7718742dbc7ea
rodrigo <rodrigo@macbook-pro-de-rodrigo.local>
2019-08-29 00:28:46 -0500

    Initial Commit

---
+++ tabla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabla
+//
+//  Created by rodrigo on 8/29/19.
+//  Copyright © 2019 rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
